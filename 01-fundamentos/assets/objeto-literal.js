3d13683405d643c145870aedc400513874665cb4
wlmc <williammcastro@msn.com>
2021-10-22 05:18:19 -0500

    Commit 3 win

---
+++ 01-fundamentos/assets/objeto-literal.js
@@ -0,0 +1,18 @@
+let personaje = {
+    nombre: 'Tony Stark',
+    codeName: 'Ironman',
+    vivo: false,
+    edad: 40,
+    coords: {
+        lat: 34.034,
+        lng: -118.70
+    },
+    trajes: ['Mark I', 'Mark V', 'Hulkbuster'],
+    direccion:{
+        zip:'10880, 90265',
+        ubicacion: 'Malibu, California',
+    },
+    'ultima-pelicula': 'Infiniti War'
+};
+
+console.log(personaje);
